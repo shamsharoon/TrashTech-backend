3710f5dde5b5a434693416ca25cd021c214f4bbf
shamsharoon <shamsuddin.haroon@ontariotechu.net>
2024-05-19 00:00:15 -0400

    testing

--- trashit/urls.py
+++ trashit/urls.py
@@ -1,6 +1,7 @@
 """
 URL configuration for trashit project.
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
